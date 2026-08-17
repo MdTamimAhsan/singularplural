94fddbb757d4a750b23ac9873a4af483ecfe9223
MdTamimAhsan <tamimahsan0824@gmail.com>
2026-08-17 13:52:03 +0600

    Fix UI styling, layout, and base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://MdTamimAhsan.github.io',
-  base: '/english-plurals',
+  base: '/singularplural',
   vite: {
     plugins: [tailwindcss()]
   }
