f95bad756b09802c25160f3ad4a72a7961a47831
MdTamimAhsan <tamimahsan0824@gmail.com>
2026-08-26 01:12:13 +0600

    chore: prepare Astro for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://MdTamimAhsan.github.io',
-  base: '/singularplural',
   vite: {
     plugins: [tailwindcss()]
   }
